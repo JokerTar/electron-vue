25eba5aedcfa2df030a974b6b592676c3a389ff0
谈荣飞 <xinxibutanrongfei@gddh365.com>
2023-09-04 17:01:40 +0800

    x1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
-	<div>vite + vue3 + tsasd</div>
+	<div>vite + vue3 + tsasd - {{ a }}</div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+const a = ref('123');
+</script>
 
 <style lang="scss"></style>
